6aef042440ecc6f982c64272e4fd6aec8b636666
Shweta Dudye <dudyeshweta@gmail.com>
2026-04-24 14:13:43 +0530

    inbuild middleware

---
+++ Day_7/customMiddleware.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const fs = require('fs');
+
+const PORT = 7010;
+const server = express();
+
+//inbuild middleware 
+//1. watchman middleware
+
+const logger = (req,res,next)=>{
+    let 
+}
