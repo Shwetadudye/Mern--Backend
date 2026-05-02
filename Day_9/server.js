27385c71fa95bbb515c3c32165996cbdaca8d5c2
Shweta Dudye <dudyeshweta@gmail.com>
2026-05-02 20:36:01 +0530

    code of the day

--- Day_9/server.js
+++ Day_9/server.js
@@ -4,20 +4,23 @@ const mongoose = require("mongoose");
 const main = async()=>{
     try{
         const connection = mongoose.connect('mongodb://127.0.0.1:27017/Shweta_DB');
-
+       
+        console.log("DB Connected✅");
         const userData= new mainModel(
             {
                 name:'shweta',
                 age: 100,
                 married:false
-            },{
-                versionKey: false
-            },
+            }
         );
 
-        await userData.save();
+       // await userData.save();
 
-        console.log("DB connected✅");
+        const data = await mainModel.find({age:100});
+        console.log("data",data);
+
+        await (await connection).disconnect();
+        console.log("DB disconnected❗");
     }catch(error){
         console.log("DB not connected❌");
         console.log(error);
@@ -28,7 +31,10 @@ const mainSchema = new mongoose.Schema({
     name: String,
     age: Number,
     married: Boolean
-});
+},{
+    versionKey:false
+}
+);
 
 const mainModel= new mongoose.model('user',mainSchema);
 
