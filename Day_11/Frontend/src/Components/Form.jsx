25662896d937411b5643b3e72dc91fa7768afa71
Shweta Dudye <dudyeshweta@gmail.com>
2026-05-10 13:25:49 +0530

    code of the day

--- Day_11/Frontend/src/Components/Form.jsx
+++ Day_11/Frontend/src/Components/Form.jsx
@@ -1,44 +1,58 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React from 'react';
 
-import { Api } from "../Utilis/Api";
+export const Form = ({ formData, handleSubmit }) => {
 
+    const [form, setForm] = React.useState(() => {
+        return formData.reduce((acc, field) => {
+            acc[field.name] = '';
+            return acc;
+        }, {});
+    });
 
-export const Signup =()=>{
-    const location = useLocation();
+    const handleChange = (e) => {
+        const { name, value } = e.target;
 
-    const [ email, setEmail]= React.useState('');
-    const [password , setPassword] = React.useState('');
-
-    console.log(location);
-
-    const handleFormSubmit=(e)=>{
-        e.preventDefault();
-        Api.post(location.pathname,{email,password}) // axios automatically stringify the value .. if we use fetch then we have to do stringify
-        .then((res)=>console.log(res))
-        .catch((err)=>console.log(err))
+        setForm((prev) => ({
+            ...prev,
+            [name]: value
+        }));
     };
 
-    return(
-        <>
-        <h1>Signup</h1>
-        <form onSubmit = {handleFormSubmit}>
-            <label htmlFor="email">Email</label>
-            <input
-               id="email"
-               type="text"
-               placeholder='enter youe email'
-               onChange={(e)=>setEmail(e.target.value)}
-               />
-            <label htmlFor="password">Password</label>
-            <input 
-               id="password"
-               type="text"
-               placeholder="Enter your password"
-               onChange={(e)=>setPassword(e.target.value)}
-               />
+    const onSubmit = (e) => {
+        e.preventDefault();
+        handleSubmit(form);
+    };
+
+    return (
+        <form
+            onSubmit={onSubmit}
+            style={{
+                border: "1px solid red",
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '10px',
+                padding: '20px'
+            }}
+        >
+            {
+                formData.map((el, i) => (
+                    <div key={i}>
+                        <label htmlFor={el.name}>{el.label}</label>
+
+                        <input
+                            type={el.type}
+                            name={el.name}
+                            id={el.name}
+                            placeholder={el.placeholder}
+                            value={form[el.name]}
+                            onChange={handleChange}
+                            required={el.required}
+                        />
+                    </div>
+                ))
+            }
+
             <button type="submit">Submit</button>
         </form>
-        </>
-    )
-}
+    );
+};
