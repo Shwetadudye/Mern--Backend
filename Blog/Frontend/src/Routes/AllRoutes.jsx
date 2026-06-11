526e0cb87fa4dd0c99554be70065961172535957
Shweta Dudye <dudyeshweta@gmail.com>
2026-06-11 11:09:11 +0530

    code of the day

--- Blog/Frontend/src/Routes/AllRoutes.jsx
+++ Blog/Frontend/src/Routes/AllRoutes.jsx
@@ -3,21 +3,6 @@ import { Home } from '../Pages/Home';
 import { Login } from '../Pages/Login';
 import { Signup } from '../Pages/Signup';
 import { BlogPage } from '../Pages/BlogPage';
-
-export const AllRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/login" element={<Login />}></Route>
-      <Route path="/signup" element={<Signup />}></Route>
-      <Route path="/blog/:id" element={<BlogPage />}></Route>
-    </Routes>
-  );
-};import { Routes, Route } from 'react-router-dom';
-import { Home } from '../Pages/Home';
-import { Login } from '../Pages/Login';
-import { Signup } from '../Pages/Signup';
-import { BlogPage } from '../Pages/BlogPage';
 import { PrivateRoutes } from '../Components/PrivateRoutes';
 import PageNotFound from '../Pages/Page404';
 import { CreateBlog } from '../Pages/CreateBlog';
